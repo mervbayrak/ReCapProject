7872870880e2f5cd54c6fb183a97d75ad5009316
MERVE BAYRAK <mervbayrak@hotmail.com>
2021-02-04 18:08:27 +0300

    Added Validation Rules

--- CarRentalSystem/CarRentalSystem.Console/Program.cs
+++ CarRentalSystem/CarRentalSystem.Console/Program.cs
@@ -12,8 +12,8 @@ namespace CarRentalSystem.ConsoleUI
             CarManager carManager = new CarManager(new EfCarDal());
             foreach (var item in carManager.GetAll())
             {
-                string strCarList = String.Format("Id: {0} Marka: {1} Renk: {2} Model: {3} Fiyat: {4} Açıklama: {5}",
-                    item.Id, item.Brand.Name, item.Color.Name, item.ModelYear, item.DailyPrice, item.Description);
+                string strCarList = String.Format("Id: {0} Name:{6} Marka: {1} Renk: {2} Model: {3} Fiyat: {4} Açıklama: {5}",
+                    item.Id, item.Brand.Name, item.Color.Name, item.ModelYear, item.DailyPrice, item.Description, item.Name);
 
                 Console.WriteLine(strCarList);
             }
