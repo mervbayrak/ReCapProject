d6b2b5b451d29244643a30b0a980fe8e159513bd
MERVE BAYRAK <mervbayrak@hotmail.com>
2021-03-13 12:58:11 +0300

    Created Components

--- CarRentalWebSystem/src/app/app.module.ts
+++ CarRentalWebSystem/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    DashboardComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
